8640cd9fd059f6d5ff772961e5e44e4680680f2a
Zachary King <zak@delfina.com>
2022-09-30 20:26:23 -0700

    🌊

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import {
+  AppBar,
+  Box,
+  CssBaseline,
   Divider,
   Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
   Toolbar,
+  Typography,
 } from "@mui/material";
-import { Mail, Inbox } from "@mui/icons-material";
+import { Mail, Inbox, Menu } from "@mui/icons-material";
 
-// import { UserSessionContext } from '../context/UserSession'
-// import { logOut } from '../api/auth'
+const drawerWidth = 240;
 
-const Logo = () => (
-  <Link to="/">
-    <div style={{ fontFamily: "'Domine', serif" }}>Brainshare Metabolism</div>
-  </Link>
+const drawer = (
+  <div>
+    <Toolbar />
+    <Divider />
+  </div>
 );
 
 export default function Navigation({
@@ -25,364 +30,104 @@ export default function Navigation({
 }: {
   children: React.ReactNode;
 }) {
-  //   const session = useContext(UserSessionContext)
-  //   const navigate = useNavigate();
-  //   const [open, setOpen] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const container =
+    window !== undefined ? () => window.document.body : undefined;
+
   return (
-    <div>
-      <Logo />
-      <Drawer>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Brainshare Metabolism
+          </Typography>
+        </Toolbar>
+        <Drawer>
+          <Toolbar />
+          <Divider />
+          <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <Inbox /> : <Mail />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
         <Toolbar />
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <Inbox /> : <Mail />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      {children}
-    </div>
+        {children}
+      </Box>
+    </Box>
   );
 }
-
-// const drawerWidth = 240;
-
-// interface Props {
-//   /**
-//    * Injected by the documentation to work in an iframe.
-//    * You won't need it on your project.
-//    */
-//   window?: () => Window;
-// }
-
-// export default function ResponsiveDrawer(props: Props) {
-//   const { window } = props;
-//   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-//   const handleDrawerToggle = () => {
-//     setMobileOpen(!mobileOpen);
-//   };
-
-//   const drawer = (
-//     <div>
-//       <Toolbar />
-//       <Divider />
-//       <List>
-//         {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List>
-//         {['All mail', 'Trash', 'Spam'].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </div>
-//   );
-
-//   const container = window !== undefined ? () => window().document.body : undefined;
-
-//   return (
-//     <Box sx={{ display: 'flex' }}>
-//       <CssBaseline />
-//       <AppBar
-//         position="fixed"
-//         sx={{
-//           width: { sm: `calc(100% - ${drawerWidth}px)` },
-//           ml: { sm: `${drawerWidth}px` },
-//         }}
-//       >
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             edge="start"
-//             onClick={handleDrawerToggle}
-//             sx={{ mr: 2, display: { sm: 'none' } }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" noWrap component="div">
-//             Responsive drawer
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <Box
-//         component="nav"
-//         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-//         aria-label="mailbox folders"
-//       >
-//         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-//         <Drawer
-//           container={container}
-//           variant="temporary"
-//           open={mobileOpen}
-//           onClose={handleDrawerToggle}
-//           ModalProps={{
-//             keepMounted: true, // Better open performance on mobile.
-//           }}
-//           sx={{
-//             display: { xs: 'block', sm: 'none' },
-//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-//           }}
-//         >
-//           {drawer}
-//         </Drawer>
-//         <Drawer
-//           variant="permanent"
-//           sx={{
-//             display: { xs: 'none', sm: 'block' },
-//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-//           }}
-//           open
-//         >
-//           {drawer}
-//         </Drawer>
-//       </Box>
-//       <Box
-//         component="main"
-//         sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-//       >
-//         <Toolbar />
-//         <Typography paragraph>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-//           tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-//           enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-//           imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-//           Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-//           Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-//           adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-//           nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-//           leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-//           feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-//           consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-//           sapien faucibus et molestie ac.
-//         </Typography>
-//         <Typography paragraph>
-//           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-//           eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-//           neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-//           tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-//           sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-//           tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-//           gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-//           et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-//           tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-//           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-//           posuere sollicitudin aliquam ultrices sagittis orci a.
-//         </Typography>
-//       </Box>
-//     </Box>
-//   );
-// }
-
-// type Anchor = "top" | "left" | "bottom" | "right";
-
-// export default function TemporaryDrawer() {
-//   const [state, setState] = React.useState({
-//     top: false,
-//     left: false,
-//     bottom: false,
-//     right: false,
-//   });
-
-//   const toggleDrawer =
-//     (anchor: Anchor, open: boolean) =>
-//     (event: React.KeyboardEvent | React.MouseEvent) => {
-//       if (
-//         event.type === "keydown" &&
-//         ((event as React.KeyboardEvent).key === "Tab" ||
-//           (event as React.KeyboardEvent).key === "Shift")
-//       ) {
-//         return;
-//       }
-
-//       setState({ ...state, [anchor]: open });
-//     };
-
-//   const list = (anchor: Anchor) => (
-//     <Box
-//       sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
-//       role="presentation"
-//       onClick={toggleDrawer(anchor, false)}
-//       onKeyDown={toggleDrawer(anchor, false)}
-//     >
-//       <List>
-//         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Divider />
-//       <List>
-//         {["All mail", "Trash", "Spam"].map((text, index) => (
-//           <ListItem key={text} disablePadding>
-//             <ListItemButton>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItemButton>
-//           </ListItem>
-//         ))}
-//       </List>
-//     </Box>
-//   );
-
-//   return (
-//     <div>
-//       {(["left", "right", "top", "bottom"] as const).map((anchor) => (
-//         <React.Fragment key={anchor}>
-//           <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
-//           <Drawer
-//             anchor={anchor}
-//             open={state[anchor]}
-//             onClose={toggleDrawer(anchor, false)}
-//           >
-//             {list(anchor)}
-//           </Drawer>
-//         </React.Fragment>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default function App() {
-//   const [open, setOpen] = React.useState(false);
-
-//   const toggleDrawer = (event) => {
-//     if (
-//       event.type === "keydown" &&
-//       (event.key === "Tab" || event.key === "Shift")
-//     ) {
-//       return;
-//     }
-
-//     setOpen(!open);
-//   };
-
-//   return (
-//     <div className={classes.root}>
-//       <CssBaseline />
-//       <AppBar position="fixed" className={classes.appBar}>
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             edge="start"
-//             onClick={toggleDrawer}
-//             className={classes.menuButton}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" noWrap>
-//             Responsive Drawer Example
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <Drawer
-//         className={classes.drawer}
-//         variant={isMdUp ? "permanent" : "temporary"}
-//         classes={{
-//           paper: classes.drawerPaper,
-//         }}
-//         anchor="left"
-//         open={open}
-//         onClose={toggleDrawer}
-//       >
-//         <div className={classes.toolbar} />
-//         <Divider />
-//         <List>
-//           {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-//             <ListItem button key={text}>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItem>
-//           ))}
-//         </List>
-//         <Divider />
-//         <List>
-//           {["All mail", "Trash", "Spam"].map((text, index) => (
-//             <ListItem button key={text}>
-//               <ListItemIcon>
-//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-//               </ListItemIcon>
-//               <ListItemText primary={text} />
-//             </ListItem>
-//           ))}
-//         </List>
-//       </Drawer>
-//       <main className={classes.content}>
-//         <Toolbar />
-//         <Typography component="h2" variant="h6" gutterBottom>
-//           On small and extra-small screens the sidebar/drawer is temporary and
-//           can be opened via the menu icon in the toolbar.
-//         </Typography>
-//         <Typography component="h2" variant="h6" gutterBottom>
-//           On medium, large, and extra-large screens the sidebar/drawer is
-//           permanent and there is no menu icon in the toolbar.
-//         </Typography>
-//         <hr />
-//         <Typography paragraph>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-//           dolor purus non enim praesent elementum facilisis leo vel. Risus at
-//           ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-//           quisque non tellus. Convallis convallis tellus id interdum velit
-//           laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-//           adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-//           integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-//           eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-//           quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-//           vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-//           lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-//           faucibus et molestie ac.
-//         </Typography>
-//         <Typography paragraph>
-//           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-//           ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-//           elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-//           sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-//           mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-//           risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-//           purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-//           tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-//           morbi tristique senectus et. Adipiscing elit duis tristique
-//           sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-//           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-//           posuere sollicitudin aliquam ultrices sagittis orci a.
-//         </Typography>
-//       </main>
-//     </div>
-//   );
-// }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import PageLayout from "./components/PageLayout";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PageLayout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
