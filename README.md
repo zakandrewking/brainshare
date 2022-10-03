b0aa5cf6057c009e62e0ae0ce4d482ffceca238d
Zachary King <zak@delfina.com>
2022-10-03 15:20:04 -0700

    🍄

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ Brainshare Metabolism is in early stage development, but feel free to peruse the
 
 - [ ] REST & GraphQL APIs for InChI
 
+# Run locally
+
+1. Run `supabase start` (terminal or vscode task)
+
+1. Make a copy of frontend/.env.example called frontend/.env
+
+1. Copy the Anon Key and API URL into frontend/.env
+
 # Troubleshooting
 
 ## must be member of role "supabase_admin" (SQLSTATE 42501); while executing migration
--- frontend/src/components/LogIn.tsx
+++ frontend/src/components/LogIn.tsx
@@ -1,9 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 
+if (process.env.REACT_APP_ANON_KEY === undefined)
+  throw Error("Missing environment variable REACT_APP_ANON_KEY");
+if (process.env.REACT_APP_API_URL === undefined)
+  throw Error("Missing environment variable REACT_APP_API_URL");
+
 const supabase = createClient(
-  "http://localhost:54321",
-  "" // TODO secrets
+  process.env.REACT_APP_API_URL,
+  process.env.REACT_APP_ANON_KEY
 );
 
 export default function LogIn() {
