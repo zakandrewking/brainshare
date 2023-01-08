3fa55e6ae773483c5c64d33254b59ea76dc69e5c
Zachary King <zak@delfina.com>
2023-01-07 16:40:29 -0800

    🕚

--- supabase/migrations/20230107191313_search-sorted.sql
+++ supabase/migrations/20230107191313_search-sorted.sql
@@ -1,49 +1,37 @@
 CREATE OR REPLACE FUNCTION public.search(query text) RETURNS JSONB AS $$
 DECLARE
-    result jsonb;
+    ret jsonb;
 BEGIN
-    SELECT jsonb_build_object('results',
-        COALESCE(
-            (SELECT jsonb_agg(r) FROM
-                (SELECT DISTINCT ON (chemical.id)
-                    chemical.id, chemical.name, 1 as score, 'chemical' as resource
-                    FROM chemical
-                    LEFT JOIN SYNONYM on chemical_id = chemical.id
-                    WHERE inchi = query OR inchi_key = query OR synonym.value = query
-                    LIMIT 10) as r),
+    SELECT jsonb_build_object('results', jsonb_agg(results)) INTO ret
+    FROM (SELECT * FROM jsonb_array_elements(
+        COALESCE((SELECT jsonb_agg(r) FROM
+            (SELECT DISTINCT ON (chemical.id)
+                chemical.id, chemical.name, 1 as score, 'chemical' as resource
+                FROM chemical
+                LEFT JOIN SYNONYM on chemical_id = chemical.id
+                WHERE inchi = query OR inchi_key = query OR synonym.value = query
+                LIMIT 10) as r),
             '[]'::jsonb
-        ) || COALESCE(
-            (SELECT jsonb_agg(r) FROM
-                (SELECT chemical.id, chemical.name, chemical.inchi_key, word_similarity(query, chemical.name) AS score, 'chemical' as resource
-                    FROM chemical
-                    WHERE query <% chemical.name
-                    LIMIT 100) as r),
+        ) || COALESCE((SELECT jsonb_agg(r) FROM
+            (SELECT chemical.id, chemical.name, word_similarity(query, chemical.name) AS score, 'chemical' as resource
+                FROM chemical
+                WHERE query <% chemical.name
+                ORDER BY score DESC, length(chemical.name)
+                LIMIT 100) as r),
             '[]'::jsonb
-        ) || COALESCE(
-            (SELECT jsonb_agg(r) FROM
-                (SELECT species.id, species.name, word_similarity(query, species.name) AS score, 'species' as resource
-                    FROM species
-                    WHERE query <% species.name
-                    LIMIT 100) as r),
+        ) || COALESCE((SELECT jsonb_agg(r) FROM
+            (SELECT species.id, species.name, word_similarity(query, species.name) AS score, 'species' as resource
+                FROM species
+                WHERE query <% species.name
+                ORDER BY score DESC, length(species.name)
+                LIMIT 100) as r),
             '[]'::jsonb
         )
-    )
-    INTO result;
+    ) AS sub1(results) ORDER BY (results->>'score')::numeric DESC) as sub2;
 
-    RETURN result;
+    RETURN ret;
 END;
 $$ LANGUAGE plpgsql;
 
-    -- jsonb_agg(results order by (value->>'score')::numeric))
-
-    -- query_stripped text := regexp_replace(query, '[^0-9a-zA-Z]+', ' ', 'g');
-    -- RAISE NOTICE 'Query stripped %', query_stripped;
-
-    -- select
-    --     (select jsonb_agg(r) from
-    --         (select id, name, word_similarity('Diche', name) from chemical WHERE 'Diche' <% chemical.name limit 10) as r
-    --     )
-    --     ||
-    --     (select jsonb_agg(r) from
-    --         (select id, name, inchi_key, word_similarity('Diche', name) from chemical WHERE 'Diche' <% chemical.name limit 10) as r
-    --     );
+-- query_stripped text := regexp_replace(query, '[^0-9a-zA-Z]+', ' ', 'g');
+-- RAISE NOTICE 'Query stripped %', query_stripped;
--- supabase/tests.sql
+++ supabase/tests.sql
@@ -15,51 +15,51 @@ SELECT 'chebi_id', '15903', id
 FROM rows;
 
 select results_eq(
-    'select * from search('''')',
+    'select search('''')',
     'SELECT jsonb_build_object(''results'', jsonb_agg(r)) FROM (select 1 where false) as r',
     'empty search returns no results'
 );
 
 select results_eq(
-    'select CAST(results AS jsonb) from search(''15903'')',
-    'select jsonb_agg(r) as results from (select *, 1 as score from chemical where chemical.name = ''beta-D-glucose'') as r',
+    'SELECT search(''15903'')',
+    'SELECT jsonb_build_object(''results'', jsonb_agg(r)) FROM (select id, name, 1 as score, ''chemical'' as resource from chemical where chemical.name = ''beta-D-glucose'') as r',
     'exact search by chebi id'
 );
 
 select results_eq(
-    'select CAST(results AS jsonb) from search(''InChI=1S/C6H12O6/c7-1-2-3(8)4(9)5(10)6(11)12-2/h2-11H,1H2/t2-,3-,4+,5-,6-/m1/s1'')',
-    'select jsonb_agg(r) as results from (select *, 1 as score from chemical where chemical.name = ''beta-D-glucose'') as r',
+    'select search(''InChI=1S/C6H12O6/c7-1-2-3(8)4(9)5(10)6(11)12-2/h2-11H,1H2/t2-,3-,4+,5-,6-/m1/s1'')',
+    'SELECT jsonb_build_object(''results'', jsonb_agg(r)) FROM (select id, name, 1 as score, ''chemical'' as resource from chemical where chemical.name = ''beta-D-glucose'') as r',
     'exact search by inchi'
 );
 
 select results_eq(
-    'select CAST(results AS jsonb) from search(''beta-D-glucose'')',
-    'select jsonb_agg(r) as results from (select *, 1 as score from chemical where chemical.name = ''beta-D-glucose'') as r',
+    'select search(''beta-D-glucose'')',
+    'SELECT jsonb_build_object(''results'', jsonb_agg(r)) FROM (select id, name, 1 as score, ''chemical'' as resource from chemical where chemical.name = ''beta-D-glucose'') as r',
     'exact search by name'
 );
 
 select * from finish();
 rollback;
 
--- query name sorting
+-- -- query name sorting
 
 begin;
 select plan( 1 );
 
 INSERT INTO chemical (inchi, inchi_key, name)
-VALUES ('test-inchi-1', 'test-inchi-key-1', 'alpha-D-glucose'),
-       ('test-inchi-2', 'test-inchi-key-2', 'beta-D-glucose');
+VALUES ('test-inchi-2', 'test-inchi-key-2', 'beta-D-glucose'),
+       ('test-inchi-1', 'test-inchi-key-1', 'alpha-D-glucose');
 
 select results_eq(
     'select jsonb_path_query(res, ''$.results[*].name'') #>> ''{}'' as name from search(''a-D-glucose'') as res',
     'select name from chemical where inchi like ''test-inchi-%'' ORDER BY inchi',
-    'exact search by name'
+    'search by name sorting'
 );
 
 select * from finish();
 rollback;
 
--- query name sorting; subset
+-- -- query name sorting; subset
 
 begin;
 select plan( 1 );
@@ -72,7 +72,7 @@ VALUES ('test-inchi-3', 'test-inchi-key-3', 'Dianthramine'),
        ('test-inchi-1', 'test-inchi-key-1', 'hexane-1,6-diamine');
 
 select results_eq(
-    'select jsonb_path_query(res, ''$.results[*].name'') #>> ''{}'' as name from search(''diamine'') as res',
+    'select jsonb_path_query(res, ''$.results[*].name'') #>> ''{}'' as name from search(''diam'') as res',
     'select name from chemical where inchi like ''test-inchi-%'' ORDER BY inchi',
     'exact search by name'
 );
