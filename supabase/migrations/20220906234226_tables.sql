0a183d2408ae9f32f3f7569328f9d4adc09fff0b
Zachary King <zak@delfina.com>
2023-03-10 09:13:00 -0800

    🎃

--- supabase/migrations/20220906234226_tables.sql
+++ supabase/migrations/20220906234226_tables.sql
@@ -14,6 +14,23 @@ CREATE POLICY "Anyone can read profiles" ON public.profile
 CREATE POLICY "Authenticated user can manage their profile" ON public.profile
     FOR ALL USING (auth.uid() = id);
 
+-- roles
+
+CREATE TABLE public.user_role (
+    user_id UUID REFERENCES public.profile(id) ON DELETE CASCADE,
+    role TEXT NOT NULL CHECK (role in ('admin', 'curator')),
+    CONSTRAINT user_role_pkey PRIMARY KEY (user_id, role)
+);
+
+ALTER TABLE public.user_role ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "Authenticated user can read their own role" on public.user_role
+    FOR SELECT USING (auth.uid() = user_id);
+-- CREATE POLICY "Admin can manage roles" ON public.user_role USING (
+--     EXISTS(
+--         SELECT user_id FROM public.user_role WHERE user_id = auth.uid() AND role = 'admin'
+--     )
+-- );
+
 -----------
 -- Utils --
 -----------
