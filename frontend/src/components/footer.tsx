95a1e749db39f778944eb75b4b3f2a4821f8ceb1
Zachary King <zak@delfina.com>
2024-09-27 12:21:27 -0700

    bug fix

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -12,11 +12,11 @@ export function FooterText({ className, ...props }: React.ComponentProps<"p">) {
       )}
       {...props}
     >
-      Open source AI chatbot built with{" "}
-      <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and based
-      on{" "}
-      <ExternalLink href="https://github.com/vercel/ai">
-        Vercel AI SDK
+      AI chatbot built with{" "}
+      <ExternalLink href="https://sdk.vercel.ai">Vercel AI SDK</ExternalLink>{" "}
+      and based on{" "}
+      <ExternalLink href="https://vercel.com/templates/next.js/nextjs-ai-chatbot">
+        Next.js AI Chatbot
       </ExternalLink>
       .
     </p>
--- frontend/src/lib/chat/actions.tsx
+++ frontend/src/lib/chat/actions.tsx
@@ -512,89 +512,89 @@ export const AI = createAI({
   },
   initialUIState,
   initialAIState,
-  //   onGetUIState: async () => {
-  //     "use server";
+  // onGetUIState: async () => {
+  //   "use server";
 
-  //     const session = await auth();
+  // const session = await auth();
 
-  //     if (session && session.user) {
-  //       const aiState = getAIState() as Chat;
+  //   if (session && session.user) {
+  //     const aiState = getAIState() as Chat;
 
-  //       if (aiState) {
-  //         const uiState = getUIStateFromAIState(aiState);
-  //         return uiState;
-  //       }
-  //     } else {
-  //       return;
+  //     if (aiState) {
+  //       const uiState = getUIStateFromAIState(aiState);
+  //       return uiState;
   //     }
+  //   } else {
+  //     return;
+  //   }
   // },
-  //   onSetAIState: async ({ state }) => {
-  //     "use server";
+  // onSetAIState: async ({ state }) => {
+  //   "use server";
 
-  //     const session = await auth();
+  //   const session = await auth();
 
-  //     if (session && session.user) {
-  //       const { chatId, messages } = state;
+  //   if (session && session.user) {
+  // const { chatId, messages } = state;
 
-  //       const createdAt = new Date();
-  //       const userId = session.user.id as string;
-  //       const path = `/chat/${chatId}`;
+  // const createdAt = new Date();
+  // const userId = session.user.id as string;
+  // const path = `/chat/${chatId}`;
 
-  //       const firstMessageContent = messages[0].content as string;
-  //       const title = firstMessageContent.substring(0, 100);
+  // const firstMessageContent = messages[0].content as string;
+  // const title = firstMessageContent.substring(0, 100);
 
-  //       const chat: Chat = {
-  //         id: chatId,
-  //         title,
-  //         userId,
-  //         createdAt,
-  //         messages,
-  //         path,
-  //       };
+  // const chat: Chat = {
+  //   id: chatId,
+  //   title,
+  //   userId,
+  //   createdAt,
+  //   messages,
+  //   path,
+  // };
 
-  //       await saveChat(chat);
-  //     } else {
-  //       return;
-  //     }
-  //   },
+  // await saveChat(chat);
+  // } else {
+  //   return;
+  // }
+  // },
 });
 
-// export const getUIStateFromAIState = (aiState: Chat) => {
-//   return aiState.messages
-//     .filter((message) => message.role !== "system")
-//     .map((message, index) => ({
-//       id: `${aiState.chatId}-${index}`,
-//       display:
-//         message.role === "tool" ? (
-//           message.content.map((tool) => {
-//             return tool.toolName === "listStocks" ? (
-//               <BotCard>
-//                 {/* TODO: Infer types based on the tool result*/}
-//                 {/* @ts-expect-error */}
-//                 <Stocks props={tool.result} />
-//               </BotCard>
-//             ) : tool.toolName === "showStockPrice" ? (
-//               <BotCard>
-//                 {/* @ts-expect-error */}
-//                 <Stock props={tool.result} />
-//               </BotCard>
-//             ) : tool.toolName === "showStockPurchase" ? (
-//               <BotCard>
-//                 {/* @ts-expect-error */}
-//                 <Purchase props={tool.result} />
-//               </BotCard>
-//             ) : tool.toolName === "getEvents" ? (
-//               <BotCard>
-//                 {/* @ts-expect-error */}
-//                 <Events props={tool.result} />
-//               </BotCard>
-//             ) : null;
-//           })
-//         ) : message.role === "user" ? (
-//           <UserMessage>{message.content as string}</UserMessage>
-//         ) : message.role === "assistant" &&
-//           typeof message.content === "string" ? (
-//           <BotMessage content={message.content} />
-//         ) : null,
-//     }));
-// };
+export const getUIStateFromAIState = (aiState: Chat) => {
+  return aiState.messages
+    .filter((message) => message.role !== "system")
+    .map((message, index) => ({
+      id: `${aiState.chatId}-${index}`,
+      display:
+        message.role === "tool" ? (
+          message.content.map((tool) => {
+            return tool.toolName === "listStocks" ? (
+              <BotCard>
+                {/* TODO: Infer types based on the tool result*/}
+                {/* @ts-expect-error */}
+                <Stocks props={tool.result} />
+              </BotCard>
+            ) : tool.toolName === "showStockPrice" ? (
+              <BotCard>
+                {/* @ts-expect-error */}
+                <Stock props={tool.result} />
+              </BotCard>
+            ) : tool.toolName === "showStockPurchase" ? (
+              <BotCard>
+                {/* @ts-expect-error */}
+                <Purchase props={tool.result} />
+              </BotCard>
+            ) : tool.toolName === "getEvents" ? (
+              <BotCard>
+                {/* @ts-expect-error */}
+                <Events props={tool.result} />
+              </BotCard>
+            ) : null;
+          })
+        ) : message.role === "user" ? (
+          <UserMessage>{message.content as string}</UserMessage>
+        ) : message.role === "assistant" &&
+          typeof message.content === "string" ? (
+          <BotMessage content={message.content} />
+        ) : null,
+    }));
+};
