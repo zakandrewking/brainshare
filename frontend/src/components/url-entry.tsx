97e3659804672b4934549e1c51e3a8e203452727
Zachary King <zak@delfina.com>
2024-11-14 16:28:36 -0800

    normalize github urls

--- frontend/src/components/url-entry.tsx
+++ frontend/src/components/url-entry.tsx
@@ -8,6 +8,34 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Stack } from "./ui/stack";
 
+/**
+ * Turn any CSV URL into a raw URL. Raises an error if the URL is not a valid
+ * Github CSV URL.
+ */
+function normalizeUrl(url: string) {
+  // handle
+  // https://raw.githubusercontent.com/sher1203/Protein-Folding/refs/heads/main/protein_interactions.csv
+  if (url.startsWith("https://raw.githubusercontent.com/")) {
+    return url;
+  } else if (url.startsWith("https://github.com/") && url.includes("/blob/")) {
+    // handle
+    // https://github.com/sher1203/Protein-Folding/blob/main/protein_interactions.csv
+    const parts = url.replace("https://github.com/", "").split("/");
+    const repo = parts.slice(0, 2).join("/");
+    const filename = parts.slice(4).join("/");
+    return `https://raw.githubusercontent.com/${repo}/refs/heads/main/${filename}`;
+  } else if (url.startsWith("https://github.com/") && url.includes("/raw/")) {
+    // handle
+    // https://github.com/sher1203/Protein-Folding/raw/main/protein_interactions.csv
+    const parts = url.replace("https://github.com/", "").split("/");
+    const repo = parts.slice(0, 2).join("/");
+    const filename = parts.slice(4).join("/");
+    return `https://raw.githubusercontent.com/${repo}/refs/heads/main/${filename}`;
+  } else {
+    throw new Error("Invalid GitHub CSV URL");
+  }
+}
+
 export default function UrlEntry() {
   const [url, setUrl] = React.useState("");
   const [checking, setChecking] = React.useState(false);
@@ -16,25 +44,10 @@ export default function UrlEntry() {
 
   const handleGo = async () => {
     let rawUrl;
-    if (url.startsWith("https://github.com/")) {
-      setChecking(true);
-      const parts = url.replace("https://github.com/", "").split("/");
-      const repo = parts.slice(0, 2).join("/");
-      let filename;
-
-      if (parts.includes("raw")) {
-        // Handle raw URL format
-        filename = parts.slice(5).join("/");
-        rawUrl = `https://raw.githubusercontent.com/${repo}/refs/heads/${filename}`;
-      } else {
-        // Handle blob URL format
-        filename = parts.slice(4).join("/");
-        rawUrl = `https://raw.githubusercontent.com/${repo}/refs/heads/main/${filename}`;
-      }
-    } else if (url.startsWith("https://raw.githubusercontent.com/")) {
-      rawUrl = url;
-    } else {
-      toast.error("Invalid URL", {
+    try {
+      rawUrl = normalizeUrl(url);
+    } catch (error) {
+      toast.error("Invalid GitHub CSV URL", {
         description: "Please enter a valid GitHub URL.",
         dismissible: true,
       });
