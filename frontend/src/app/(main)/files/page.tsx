79a48942ba75e500e2bc0f8615780c872d3ba6a3
Zachary King <zak@delfina.com>
2024-12-11 12:40:02 -0800

    add the uploader

--- frontend/src/app/(main)/files/page.tsx
+++ frontend/src/app/(main)/files/page.tsx
@@ -1,21 +1,17 @@
 /**
- * Files page. Files can be used in multiple apps, and RBAC is handled separately.
+ * Files page.
  */
 
 import { Metadata } from "next";
 
 import FileDrag from "@/components/file-drag";
 import Container from "@/components/ui/container";
-import {
-  List,
-  ListItem,
-  ListItemActions,
-  ListItemContent,
-} from "@/components/ui/list";
+import { List, ListItem, ListItemActions, ListItemContent } from "@/components/ui/list";
 import { Stack } from "@/components/ui/stack";
 import { H3 } from "@/components/ui/typography";
 import { getSupabase } from "@/lib/supabaseServer";
 
+import AppFileUploader from "../app/[id]/uploader";
 import DeleteFileButton from "./DeleteFileButton";
 
 export const metadata: Metadata = {
@@ -35,8 +31,7 @@ export default async function FileList() {
       <FileDrag>
         <Stack direction="col" gap={2} alignItems="start">
           <H3>File list</H3>
-          {/* use the newer Uploader in the app/ directory instead */}
-          {/* <Uploader /> */}
+          <AppFileUploader appId={"TODO"} />
           <List>
             {files?.map((file) => (
               <ListItem key={file.id}>
