9f9f78830ec2babe649a4592978f02f7b5677d0d
Zachary King <zak@delfina.com>
2025-01-27 10:08:03 -0800

    skip backend

--- frontend/src/config/ConfigProvider.tsx
+++ frontend/src/config/ConfigProvider.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import axios from "axios";
 import { ReactNode } from "react";
 
-import { OpenAPI } from "@/client";
+import axios from "axios";
 
 export default function ConfigProvider({ children }: { children: ReactNode }) {
   // Backend Config
-  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-  if (!backendUrl) {
-    throw Error("Missing environment variable NEXT_PUBLIC_BACKEND_URL");
-  }
+  // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+  // if (!backendUrl) {
+  //   throw Error("Missing environment variable NEXT_PUBLIC_BACKEND_URL");
+  // }
 
-  OpenAPI.BASE = backendUrl;
+  // OpenAPI.BASE = backendUrl;
   axios.defaults.timeout = 8000;
 
   return <>{children}</>;
