1c97f6808214d0045aaa7f17c4190b26e1364820
Zachary King <zak@delfina.com>
2025-02-21 15:18:18 -0800

    eyes

--- frontend/src/components/empty-screen.tsx
+++ frontend/src/components/empty-screen.tsx
@@ -7,8 +7,18 @@ export function EmptyScreen() {
   return (
     <Stack direction="col" alignItems="center" className="w-full" gap={4}>
       <Image src={robotCsv} alt="robot-csv" className="w-56" priority />
-      <div className="text-2xl font-bold mt-4">
-        Hello, human! Drop your CSVs here, and I&apos;ll build you some charts.
+      <div className="text-2xl font-bold mt-4 flex flex-row items-center gap-4">
+        <pre className="text-sm">
+          {`
+ ____     ____
+|  0 |   | 0  |
+'___/     \\___'
+  `}
+        </pre>
+        <div>Hello, human.</div>
+      </div>
+      <div className="text-2xl font-bold">
+        Drop your CSVs here, and I&apos;ll build you some charts.
       </div>
     </Stack>
   );
