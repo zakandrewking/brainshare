40ecde1ac049f1e40533ab424f4a6b0a55f82a12
Zachary King <zak@delfina.com>
2025-03-06 13:29:42 -0800

    more uv magic

--- scripts/sqlalchemy-generate-types.sh
+++ scripts/sqlalchemy-generate-types.sh
@@ -5,6 +5,6 @@
 cd backend/backend \
     && poetry run sqlacodegen postgresql://postgres:postgres@localhost:54322/postgres \
        --generator declarative --outfile models.py \
-    && poetry run black models.py \
+    && uv run black models.py \
     && sed -i.bak '/ARRAY,/d' models.py \
     && sed -i.bak 's/from sqlalchemy import (/from sqlalchemy.dialects.postgresql import ARRAY\n&/' models.py
